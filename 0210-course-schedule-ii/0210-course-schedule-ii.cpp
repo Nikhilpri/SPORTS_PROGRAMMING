66e1b3c69daa7d1843774f1e70622d9f2dc8e49e
Nikhil Priyadarshan <101184556+Nikhilpri@users.noreply.github.com>
2024-09-04 22:53:27 +0530

    Time: 13 ms (77.63%), Space: 17.3 MB (70.59%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int>indeg(n,0);
+         vector<vector<int>> adj(n);
+    
+        for (auto& p : pre) {
+            adj[p[1]].push_back(p[0]);
+            indeg[p[0]]++;
+        }
+        queue<int>q;
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int x=q.front();
+            q.pop();
+            ans.push_back(x);
+            for(auto it:adj[x]){
+                indeg[it]--;
+                if(indeg[it]==0){
+                     q.push(it);
+                }
+            }
+
+        }
+        return ans.size()==n?ans:vector<int>();
+    }
+};
