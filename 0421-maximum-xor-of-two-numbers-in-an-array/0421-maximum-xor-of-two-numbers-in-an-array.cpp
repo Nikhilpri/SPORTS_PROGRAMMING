9fbb55e549859a7d85ae87e8460894e957d7accc
Nikhil Priyadarshan <101184556+Nikhilpri@users.noreply.github.com>
2024-10-12 18:37:56 +0530

    Time: 631 ms (30.28%), Space: 244.4 MB (21.02%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    class Node{
+        public:
+        Node* links[2]={nullptr};
+        bool endofnum=false;
+        bool iscon(int i){
+            return links[i]!=nullptr;
+        }
+        Node* get(int i){
+            return links[i];
+        }
+        void put(int i,Node* newnode){
+            links[i]=newnode;
+        }
+    };
+    class Trie{
+        public:
+        Node* root;
+        Trie(){
+            root=new Node();
+        }
+        void insert(int number){
+            bitset<32>bt=number;
+            Node* node=root;
+            for(int i=31;i>=0;i--){
+                if(node->iscon(bt[i])==false){
+                    node->put(bt[i],new Node());
+                }
+                node=node->get(bt[i]);
+            }
+            node->endofnum=true;
+        }
+         int maxi(int num) {
+            bitset<32> bt = num;
+            int ans = 0;
+            Node* node = root;
+
+            for (int i = 31; i >= 0; i--) {
+                int opposite = !bt[i]; 
+                if (node->iscon(opposite)) {
+                    ans |= (1 << i);    
+                    node = node->get(opposite); 
+                } else if (node->iscon(bt[i])) {
+                    node = node->get(bt[i]); 
+                } else {
+                    break;
+                }
+                if (node == nullptr) {
+                    break;
+                }
+            }
+            return ans;
+        }
+    };
+    int findMaximumXOR(vector<int>& nums) {
+        Trie trie;
+        for(auto it:nums){
+            trie.insert(it);
+        }
+        int ans=0;
+        for(auto it:nums){
+            ans=max(ans,trie.maxi(it));
+        }
+        return ans;
+    }
+};
