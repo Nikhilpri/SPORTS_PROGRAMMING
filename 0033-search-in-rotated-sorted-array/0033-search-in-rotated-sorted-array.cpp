a57ac6cf1f0ad0850e68a18502bac2fd4f878d1d
Nikhil Priyadarshan <101184556+Nikhilpri@users.noreply.github.com>
2024-10-22 15:57:39 +0530

    Time: 0 ms (100%), Space: 14 MB (51.46%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,13 +8,13 @@ public:
             mid=l+(r-l)/2;
             if(nums[mid]==target)return mid;
             if(nums[l]<=nums[mid]){
-                if(nums[l]<=target&&target<nums[mid]){
+                if(nums[l]<=target&&target<=nums[mid]){
                     r=mid-1;
                 }
                 else l=mid+1;
             }
             else{
-                if(nums[mid]<target&&target<=nums[r]){
+                if(nums[mid]<=target&&target<=nums[r]){
                     l=mid+1;
                 }
                 else r=mid-1;
